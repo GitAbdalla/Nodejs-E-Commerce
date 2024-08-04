62340ee5398f2505c2f75ab0438f72489c5d354b
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-08-04 03:50:49 +0300

    Handling errors outside express

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 
 dotenv.config({path:'config.env'})
+const ApiError = require('./utils/apiError')
+const globalError = require('./middlewares/errorMiddleware')
 const dbConnection= require('./config/database')
 const categoryRoute = require('./routes/categoryRoute')
+const globalError = require('./middlewares/errorMiddleware')
 
 // connect with db
 dbConnection()
@@ -29,10 +32,15 @@ if(process.env.NODE_ENV === 'development'){
 // Routes
 app.use('/api/v1/categories', categoryRoute)
 
-// Global error handling middleware
-app.use((err,req,res,next)=>{
-    res.status(500).json({err})
+app.use('*', (req,res,next)=>{
+    // const err = new Error(`Cant find this route: ${req.originalUrl}`)
+    // next(err.message)
+    next( new ApiError(`Cant find this route: ${req.originalUrl}`,400))
 })
+
+// Global error handling middleware
+app.use(globalError)
+  
 const PORT = process.env.PORT 
 app.listen(PORT,()=>{
     console.log(`App is Running on ${PORT}`)
