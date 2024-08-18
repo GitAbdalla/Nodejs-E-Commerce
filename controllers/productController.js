ba7e16276d598bb99f7213ca5b0351ef2b7ea721
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-08-19 00:05:05 +0300

    Refactor our api and added apiFeatures

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,75 +2,24 @@ const slugify = require("slugify");
 const Product = require("../models/productModel");
 const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
+const ApiFeatures = require("../utils/apiFeatures");
 
 // @desc Get list of products
 // @route GET /api/v1/products
 // @access Public
 exports.getProducts = asyncHandler(async (req, res) => {
-  // 1) Filtering
-  const queryStringObj = { ...req.query };
-  const excludesFields = ["page", "sort", "limit", "fields"];
-  excludesFields.forEach((field) => delete queryStringObj[field]);
-
-  // Apply filteration using[ gte , gt , lte , lt]
-  let queryStr = JSON.stringify(queryStringObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  // 2) pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 5;
-  const skip = (page - 1) * limit;
-
   //  Build query
-  let mongooseQuery = Product.find(JSON.parse(queryStr))
-    .skip(skip)
-    .limit(limit)
-    .populate({ path: "category", select: "name -_id" });
-
-  // 3) Sorting
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    mongooseQuery = mongooseQuery.sort(sortBy);
-  } else {
-    mongooseQuery = mongooseQuery.sort("-createAt");
-  }
-
-  // 4) Fields Limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(",").join(" ");
-    mongooseQuery = mongooseQuery.select(fields);
-  } else {
-    mongooseQuery = mongooseQuery.select("-__v");
-  }
-
-  // 5) Search
-  if (req.query.keyword) {
-    const keyword = req.query.keyword.trim();
-
-    const aggregationPipeline = [
-      {
-        $match: {
-          $or: [
-            { title: { $regex: keyword, $options: "i" } },
-            { description: { $regex: keyword, $options: "i" } },
-          ],
-        },
-      },
-    ];
-
-    const products = await Product.aggregate(aggregationPipeline);
-
-    return res.status(200).json({
-      results: products.length,
-      page,
-      data: products,
-    });
-  }
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .paginate()
+    .filter()
+    .search()
+    .limitFields()
+    .sort();
 
   //  Execute query
-  const products = await mongooseQuery;
+  const products = await apiFeatures.mongooseQuery;
 
-  res.status(200).json({ results: products.length, page, data: products });
+  res.status(200).json({ results: products.length, data: products });
 });
 
 // @desc Get specific product by id
---
+++ utils/apiFeatures.js
@@ -0,0 +1,70 @@
+class ApiFeatures {
+  constructor(mongooseQuery, queryString) {
+    this.mongooseQuery = mongooseQuery;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryStringObj = { ...this.queryString };
+    const excludesFields = ["page", "sort", "limit", "fields"];
+    excludesFields.forEach((field) => delete queryStringObj[field]);
+
+    let queryStr = JSON.stringify(queryStringObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.mongooseQuery = this.mongooseQuery.find(JSON.parse(queryStr));
+
+    return this; // Ensure chaining
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.sort(sortBy);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.sort("-createAt");
+    }
+    return this; // Ensure chaining
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.mongooseQuery = this.mongooseQuery.select(fields);
+    } else {
+      this.mongooseQuery = this.mongooseQuery.select("-__v");
+    }
+    return this; // Ensure chaining
+  }
+
+  search() {
+    if (this.queryString.keyword && this.queryString.keyword.trim()) {
+      const keyword = this.queryString.keyword.trim();
+      console.log("Search Keyword:", keyword);
+
+      this.mongooseQuery = this.mongooseQuery.find({
+        $or: [
+          { title: { $regex: keyword, $options: "i" } },
+          { description: { $regex: keyword, $options: "i" } },
+        ],
+      });
+
+      console.log("Query after search:", this.mongooseQuery);
+    } else {
+      console.log("No search keyword provided");
+    }
+
+    return this; // Ensure chaining
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 5;
+    const skip = (page - 1) * limit;
+
+    this.mongooseQuery = this.mongooseQuery.skip(skip).limit(limit);
+    return this; // Ensure chaining
+  }
+}
+
+module.exports = ApiFeatures;
